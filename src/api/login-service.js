2f9b1851532cf4bf6d63eeb19e86da466e7b060b
shadycheer <610125062@qq.com>
2021-01-16 15:02:00 +0800

    添加了store用于状态管理

--- src/api/login-service.js
+++ src/api/login-service.js
@@ -1,6 +1,6 @@
 /*
  * @author: Shadycheer
  * @Date: 2021-01-16 03:11:58
- * @LastEditTime: 2021-01-16 03:11:58
+ * @LastEditTime: 2021-01-16 03:36:51
  * @LastEditor: Shadycheer
  */
--- src/views/login/Home.vue
+++ src/views/login/Home.vue
@@ -1,6 +1,14 @@
 <!--
  * @author: Shadycheer
  * @Date: 2021-01-16 03:05:55
- * @LastEditTime: 2021-01-16 03:05:55
+ * @LastEditTime: 2021-01-16 14:50:09
  * @LastEditor: Shadycheer
 -->
+<template>
+<slot>
+  h1
+</slot>
+</template>
+
+<script>
+</script>
